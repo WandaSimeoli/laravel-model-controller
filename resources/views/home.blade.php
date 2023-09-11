06561d116f19c29caf8d57da2bfccc290974f87b
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-11 20:02:43 +0200

    Creation Controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laravel Model Controller</title>
     @vite('resources/js/app.js')
 </head>
 <body>
-    
+<h1>Laravel Model Controller</h1>
 </body>
 </html>
